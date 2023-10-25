5dcd19e022234d0c137ee9102e2c49b1b50478d2
HEM2058 <hemrajpandeyd@gmail.com>
2023-10-25 09:26:16 +0545

    working

--- shp_geojson/views.py
+++ shp_geojson/views.py
@@ -7,9 +7,9 @@ import os
 import zipfile
 import shapefile
 import json
-from .models import GeoJSONfeature # Import the GeoJSONFile model
+from .models import GeoJSONfeature  # Import the GeoJSONFile model
 from rest_framework import generics
-from pyproj import Transformer
+from pyproj import Transformer, CRS
 
 class UploadZipAPIView(APIView):
     def post(self, request):
@@ -24,7 +24,7 @@ class UploadZipAPIView(APIView):
             return Response({'error': 'zip_file is required in the POST request.'}, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = ShapefileUploadSerializer(data={'name': name, 'zip_file': zip_file})
-        
+
         if serializer.is_valid():
             zip_file = serializer.validated_data['zip_file']
 
@@ -52,49 +52,65 @@ class UploadZipAPIView(APIView):
                     print(f"Shapefile found: {shapefile_path}")
                     # Read Shapefile and convert to GeoJSON
                     shape_reader = shapefile.Reader(shapefile_path)
-                    
-                    # Use the pyproj library to create a transformer for coordinate transformation
-                    transformer = Transformer.from_crs(shape_reader.crs.srs, 'EPSG:4326', always_xy=True)
-                    
-                    fields = shape_reader.fields[1:]
-                    field_names = [field[0] for field in fields]
-                    features = []
-                    for shape_record in shape_reader.shapeRecords():
-                        # Transform the geometry to EPSG:4326 (WGS84)
-                        geometry = shape_record.shape.__geo_interface__
-                        geometry = json.loads(json.dumps(geometry, ensure_ascii=True))
-                        geometry['coordinates'] = transformer.transform(*geometry['coordinates'])
-                
-                        attributes = dict(zip(field_names, shape_record.record))
-                        features.append({
-                            'type': 'Feature',
-                            'geometry': geometry,
-                            'properties': attributes
-                        })
 
-                    # Save the converted GeoJSON data to the GeoJSONFile model
-                    geojson_file = GeoJSONfeature(name=name, geojson=geojson_data)
-                    geojson_file.save()
+                    # Read the CRS information from the .prj file
+                    prj_file_path = os.path.splitext(shapefile_path)[0] + ".prj"
+                    if os.path.isfile(prj_file_path):
+                        with open(prj_file_path, 'r') as prj_file:
+                            prj_text = prj_file.read()
+                            source_crs = CRS(prj_text)
 
-                    # Return the converted GeoJSON data
-                    return Response(geojson_data, status=status.HTTP_200_OK)
-                 
+                        # Use the pyproj library to create a transformer for coordinate transformation
+                        transformer = Transformer.from_crs(source_crs, 'EPSG:4326', always_xy=True)
+
+                        fields = shape_reader.fields[1:]
+                        field_names = [field[0] for field in fields]
+                        features = []
+                        for shape_record in shape_reader.shapeRecords():
+                            # Transform the geometry to EPSG:4326 (WGS84)
+                            geometry = shape_record.shape.__geo_interface__
+                            attributes = dict(zip(field_names, shape_record.record))
+
+                            # Check if the geometry coordinates are not empty
+                            if geometry['coordinates']:
+                                # Transform each coordinate in the geometry separately
+                                for i, coord in enumerate(geometry['coordinates']):
+                                    if len(coord) == 2:  # Ensure the coordinate has both lon and lat
+                                        lon, lat = transformer.transform(coord[0], coord[1])
+                                        geometry['coordinates'][i] = [lon, lat]
+
+                            features.append({
+                                'type': 'Feature',
+                                'geometry': geometry,
+                                'properties': attributes
+                            })
+
+                        geojson_data = {
+                            'type': 'FeatureCollection',
+                            'features': features
+                        }
+                        # Save the converted GeoJSON data to the GeoJSONFile model
+                        geojson_file = GeoJSONfeature(name=name, geojson=geojson_data)
+                        geojson_file.save()
+
+                        # Return the converted GeoJSON data
+                        return Response(geojson_data, status=status.HTTP_200_OK)
+
+                    else:
+                        print("Warning: .prj file is missing. Proceeding without specifying the source CRS.")
+                        return Response({'error': '.prj file is missing. Cannot determine source CRS.'}, status=status.HTTP_400_BAD_REQUEST)
+
                 else:
                     print("Shapefile not found in the uploaded ZIP file.")
                     return Response({'error': 'Shapefile not found in the uploaded ZIP file.'}, status=status.HTTP_400_BAD_REQUEST)
 
             finally:
-                # Clean up extracted files and temporary directory
+                # Clean up extracted files and the temporary directory
                 temp_dir.cleanup()
                 print("Temporary directory and extracted files cleaned up.")
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-
 class GeoJSONFeatureListView(generics.ListAPIView):
     queryset = GeoJSONfeature.objects.all()
     serializer_class = ConvertedDataSerializer
-
-    
